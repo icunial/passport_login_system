ec47eaacc20d7f426a1df7265718d984d3d84b07
Ignacio Cunial <igsecu@hotmail.com>
2023-09-14 16:50:12 -0300

    Configured Mongo Database connection / Checked for connection and errors

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`);
+const db = mongoose.connection;
+
+// Check DB connection
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+// Check for DB errors
+db.on("error", (error) => {
+  console.log(error);
+});
+
 // Body-Parse Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
