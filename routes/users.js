0a62bd2ebd32e1a39600a4311d84c0e6a90064a2
Ignacio Cunial <igsecu@hotmail.com>
2023-09-14 17:12:02 -0300

    Created POST /register route -> Register Process -> added validations

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,39 @@ router.get("/user", (req, res) => {
   res.send("User");
 });
 
+// Register Process
+router.post("/register", async (req, res, next) => {
+  const { email, password, password2 } = req.body;
+
+  // Validations
+
+  if (!email) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `Email is required!`,
+    });
+  }
+
+  if (!password) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `Password is required!`,
+    });
+  }
+
+  if (!password2) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `Password confirmation is required!`,
+    });
+  }
+
+  if (password !== password2) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `Password and Password Confirmation not match!`,
+    });
+  }
+});
+
 module.exports = router;
