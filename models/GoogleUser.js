29a62b4e72b14311314b265523e6edf1b0431faa
Ignacio Cunial <igsecu@hotmail.com>
2023-09-15 16:36:18 -0300

    Created GoogleUser database model sschema

---
+++ models/GoogleUser.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+// GoogleUser Schema
+const googleUserSchema = mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const GoogleUser = (module.exports = mongoose.model(
+  "GoogleUSer",
+  googleUserSchemaSchema
+));
